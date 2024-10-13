0b0f54470106156fef18ff641500729523c5432b
Yanbo Wong <40051361+StevenRCE0@users.noreply.github.com>
2024-10-13 21:11:48 +0800

    Rough code scaffold.

---
+++ src/types/im.py
@@ -0,0 +1,7 @@
+from enum import Enum, unique
+
+@unique
+class IM(Enum):
+    QQ = 0
+    Telegram = 1
+    WeChat = 2
---
+++ src/types/member.py
@@ -0,0 +1,7 @@
+from typing import Dict
+from im import IM
+
+class Member:
+    id: str
+    name: str
+    alias: Dict[IM, str]
